8de3ca6f8c317ffce9bf2ab3b654b347073066c8
Alexander Horvath <tantousha@me.com>
2018-08-02 18:21:21 +0000

    added forEach

--- waldo.js
+++ waldo.js
@@ -1,14 +1,14 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(item, index) {
+    if (item === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
-function actionWhenFound(index) {
-  console.log("Found Waldo at index " + index + "!");
+function actionWhenFound(place) {
+  console.log("Found Waldo at index " + place + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
